294c021c917b8bb0a567a7f4c380d3102b03b322
Thierry Bonato <bonatothierry10@gmail.com>
2026-05-09 21:26:04 -0300

    update

---
+++ SEM-09-Sereializacao/MusicaJSON/src/main/java/com/mycompany/musicajson/MusicaJSON.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.musicajson;
+
+/**
+ *
+ * @author tbona
+ */
+public class MusicaJSON {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
