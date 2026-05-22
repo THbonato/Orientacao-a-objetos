1d8c134b5f1996fe7da6d2b34640b6dec4daa8b4
Thierry Bonato <bonatothierry10@gmail.com>
2026-05-22 14:16:01 -0300

    Update Diretor.java

--- SEM11-Heranca/src/main/java/classes/Diretor.java
+++ SEM11-Heranca/src/main/java/classes/Diretor.java
@@ -1,13 +1,20 @@
 package classes;
 
 public class Diretor extends Funcionario{
+
+  private String senha;
+  
   public Diretor(){
     super();
+    this.senha = 0;
   }
-  public Diretor(String nome, int idade, String cpf, double salario){
+  public Diretor(String nome, int idade, String cpf, double salario, String senha){
     super(nome, idade, cpf, salario);
+    this.senha = senha;
   }
   public double getBonificacao(){
     return this.salario * 0.20;
   }
+
+  //Fazer get e set
 }
